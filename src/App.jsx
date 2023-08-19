c1e4393a134c9792428db9ca0c998cf73f86d07d
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2023-08-19 22:59:44 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Navbar from './components/navbar'
+import Hero from './components/hero'
+
+function App() {
+  
+  return (
+<div>
+  <Navbar/>
+  <Hero/>
+</div>
+
+   
+  )
+}
+
+export default App
---
+++ src/components/hero.jsx
@@ -0,0 +1,19 @@
+const Hero = ()=>{
+return(
+    
+<section className="bg-center bg-no-repeat bg-[url('https://cf.ltkcdn.net/womens-fashion/images/std/316237-480x344-fashion-brands.jpg')] bg-gray-700 bg-blend-multiply">
+    <div className="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">FIND CLOTHES THAT MATCHES YOUR DRIP</h1>
+        <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Mall-E you can find a diverse range of fabrics and threads you can drip in </p>
+        <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+            
+            <a href="#" className="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+               Shop Now
+            </a>  
+        </div>
+    </div>
+</section>
+
+)
+}
+export default Hero;
